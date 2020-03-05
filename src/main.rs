9a79e63ad1941410dcd051fe9d5a5d42bda2e24f
Seldom <ssaguUUUU@gmail.com>
2020-03-04 21:50:43 -0700

    Uninstalled Cairo, Installed ggez

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,46 @@
-extern crate cairo;
-extern crate gio;
-extern crate gtk;
-
-use std::env::args;
-use std::f64::consts::PI;
-
-use gio::prelude::*;
-use gtk::prelude::*;
-use gtk::DrawingArea;
-
-use cairo::{Context, FontSlant, FontWeight};
-
-fn build_ui(application: &gtk::Application) {
-    drawable(application, 500, 500, |_, cr| {
-        cr.set_dash(&[3., 2., 1.], 1.);
-        assert_eq!(cr.get_dash(), (vec![3., 2., 1.], 1.));
-
-        cr.scale(500f64, 500f64);
-
-        cr.set_source_rgb(250.0 / 255.0, 224.0 / 255.0, 55.0 / 255.0);
-        cr.paint();
-
-        cr.set_line_width(0.05);
-
-        // border
-        cr.set_source_rgb(0.3, 0.3, 0.3);
-        cr.rectangle(0.0, 0.0, 1.0, 1.0);
-        cr.stroke();
-
-        cr.set_line_width(0.03);
-
-        // draw circle
-        cr.arc(0.5, 0.5, 0.4, 0.0, PI * 2.);
-        cr.stroke();
-
-        // mouth
-        let mouth_top = 0.68;
-        let mouth_width = 0.38;
-
-        let mouth_dx = 0.10;
-        let mouth_dy = 0.10;
-
-        cr.move_to(0.50 - mouth_width / 2.0, mouth_top);
-        cr.curve_to(
-            0.50 - mouth_dx,
-            mouth_top + mouth_dy,
-            0.50 + mouth_dx,
-            mouth_top + mouth_dy,
-            0.50 + mouth_width / 2.0,
-            mouth_top,
-        );
-
-        println!("Extents: {:?}", cr.fill_extents());
-
-        cr.stroke();
-
-        let eye_y = 0.38;
-        let eye_dx = 0.15;
-        cr.arc(0.5 - eye_dx, eye_y, 0.05, 0.0, PI * 2.);
-        cr.fill();
-
-        cr.arc(0.5 + eye_dx, eye_y, 0.05, 0.0, PI * 2.);
-        cr.fill();
-
-        Inhibit(false)
-    });
-
-    drawable(application, 500, 500, |_, cr| {
-        cr.scale(500f64, 500f64);
-
-        cr.select_font_face("Sans", FontSlant::Normal, FontWeight::Normal);
-        cr.set_font_size(0.35);
-
-        cr.move_to(0.04, 0.53);
-        cr.show_text("Hello");
-
-        cr.move_to(0.27, 0.65);
-        cr.text_path("void");
-        cr.set_source_rgb(0.5, 0.5, 1.0);
-        cr.fill_preserve();
-        cr.set_source_rgb(0.0, 0.0, 0.0);
-        cr.set_line_width(0.01);
-        cr.stroke();
-
-        cr.set_source_rgba(1.0, 0.2, 0.2, 0.6);
-        cr.arc(0.04, 0.53, 0.02, 0.0, PI * 2.);
-        cr.arc(0.27, 0.65, 0.02, 0.0, PI * 2.);
-        cr.fill();
-
-        Inhibit(false)
-    });
-}
+use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::event::{self, EventHandler};
 
 fn main() {
-    let application = gtk::Application::new(
-        Some("com.Seldom.poly_phy"),
-        Default::default(),
-    )
-    .expect("Initialization failed...");
+    // Make a Context.
+    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+		.build()
+		.expect("aieee, could not create ggez context!");
 
-    application.connect_activate(|app| {
-        build_ui(app);
-    });
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut my_game = MyGame::new(&mut ctx);
 
-    application.run(&args().collect::<Vec<_>>());
+    // Run!
+    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occurred: {}", e)
+    }
 }
 
-pub fn drawable<F>(application: &gtk::Application, width: i32, height: i32, draw_fn: F)
-where
-    F: Fn(&DrawingArea, &Context) -> Inhibit + 'static,
-{
-    let window = gtk::ApplicationWindow::new(application);
-    let drawing_area = Box::new(DrawingArea::new)();
+struct MyGame {
+    // Your state here...
+}
 
-    drawing_area.connect_draw(draw_fn);
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            // ...
+        }
+    }
+}
 
-    window.set_default_size(width, height);
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
 
-    window.add(&drawing_area);
-    window.show_all();
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::WHITE);
+        // Draw code here...
+        graphics::present(ctx)
+    }
 }
