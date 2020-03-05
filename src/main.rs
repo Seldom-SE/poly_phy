8f1071c8ef5a470f99eb5e79d312a8df6f451ba5
Seldom <ssaguUUUU@gmail.com>
2020-03-05 10:58:02 -0700

    Display to Screen

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,215 @@
-use ggez::{graphics, Context, ContextBuilder, GameResult};
-use ggez::event::{self, EventHandler};
+//! An Asteroids-ish example game to show off ggez.
+//! The idea is that this game is simple but still
+//! non-trivial enough to be interesting.
 
-fn main() {
-    // Make a Context.
-    let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
-		.build()
-		.expect("aieee, could not create ggez context!");
+use ggez;
+use ggez::conf;
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
+use ggez::graphics;
+use ggez::graphics::Mesh;
+use ggez::graphics::Color;
+use ggez::graphics::DrawMode;
+use ggez::timer;
+use ggez::{Context, ContextBuilder, GameResult};
+use nalgebra as na;
+use mint;
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
-    let mut my_game = MyGame::new(&mut ctx);
+type Point2 = na::Point2<f32>;
 
-    // Run!
-    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
-        Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occurred: {}", e)
-    }
+// Helper functions
+
+// Actors
+
+#[derive(Debug)]
+enum ActorType {
+    Floating,
+    Polygon,
 }
 
-struct MyGame {
-    // Your state here...
+#[derive(Debug)]
+struct Actor {
+    tag: ActorType,
+    vertices: Vec<Point2>,
 }
 
-impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
-        MyGame {
-            // ...
+// Constructor Constants
+
+// Constructors
+
+fn default_polygons () -> Vec<Actor> {
+    let mut polygons = Vec::new();
+    polygons.push(Actor {
+        tag: ActorType::Polygon,
+        vertices: {
+            let mut vertices = Vec::new();
+            vertices.push(Point2::new(100., 100.));
+            vertices.push(Point2::new(200., 200.));
+            vertices.push(Point2::new(300., 100.));
+            vertices.push(Point2::new(300., 300.));
+            vertices.push(Point2::new(100., 300.));
+            vertices
+        }
+    });
+    polygons
+}
+
+// Physics Constants
+
+// Physics
+
+// Input State
+
+#[derive(Debug)]
+struct InputState {
+    mouse_x: f32,
+    mouse_y: f32,
+    action: bool,
+    draw: bool,
+}
+
+impl Default for InputState {
+    fn default() -> Self {
+        InputState {
+            mouse_x: 0.0,
+            mouse_y: 0.0,
+            action: false,
+            draw: false,
         }
     }
 }
 
-impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
+// Main State
+
+struct MainState {
+    floating: Option<Actor>,
+    polygons: Vec<Actor>,
+    screen_width: f32,
+    screen_height: f32,
+    input: InputState,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        print_instructions();
+
+        let (width, height) = graphics::drawable_size(ctx);
+        let s = MainState {
+            floating: None,
+            polygons: default_polygons(),
+            screen_width: width,
+            screen_height: height,
+            input: InputState::default(),
+        };
+
+        Ok(s)
+    }
+}
+
+// Utility
+
+fn print_instructions() {
+    println!();
+    println!("Welcome to poly_phy.");
+    println!();
+    println!("How to use:");
+    println!("No features yet!");
+    println!();
+}
+
+fn draw_actor(ctx: &mut Context, actor: &Actor,) -> GameResult {
+    let polygon = Mesh::new_polygon(ctx,
+        DrawMode::fill(),
+        &actor.vertices.iter().map(|point| mint::Point2::from_slice(&[point.x, point.y])).collect::<Vec<mint::Point2<f32>>>()[..],
+        Color::new(1., 1., 1., 1.));
+    match &polygon {
+        Ok(polygon) => graphics::draw(ctx,
+            polygon,
+            graphics::DrawParam::new()),
+        Err(err) => Err(err.clone()),
+    }
+}
+
+// Event Handling
+
+impl EventHandler for MainState {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        const DESIRED_FPS: u32 = 60;
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            let seconds = 1.0 / (DESIRED_FPS as f32);
+
+            // Handle Input
+
+            // Update Physics
+
+            // Handle Collisions
+        }
+
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, graphics::WHITE);
-        // Draw code here...
-        graphics::present(ctx)
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::BLACK);
+
+        // Loop over all objects drawing them...
+        {
+            let floating = &self.floating;
+            if let Some(actor) = floating {
+                draw_actor(ctx, actor)?;
+            }
+
+            for polygon in &self.polygons {
+                draw_actor(ctx, polygon)?;
+            }
+        }
+
+        // Draw UI
+
+        // Flip Screen
+        graphics::present(ctx)?;
+
+        // Yield
+        timer::yield_now();
+        Ok(())
     }
+
+    // Key Events
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: KeyCode,
+        _: KeyMods,
+        _: bool,
+    ) {
+        match keycode {
+            KeyCode::LShift => {
+                self.input.draw = true;
+            }
+            KeyCode::Escape => event::quit(ctx),
+            _ => (), // Do nothing
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _: KeyMods) {
+        match keycode {
+            KeyCode::LShift => {
+                self.input.draw = false;
+            }
+            _ => (), // Do nothing
+        }
+    }
+}
+
+// Main
+
+pub fn main() -> GameResult {
+    let cb = ContextBuilder::new("poly_phy", "ggez")
+        .window_setup(conf::WindowSetup::default().title("poly_phy"))
+        .window_mode(conf::WindowMode::default().dimensions(640.0, 480.0));
+
+    let (ctx, events_loop) = &mut cb.build()?;
+
+    let game = &mut MainState::new(ctx)?;
+    event::run(ctx, events_loop, game)
 }
